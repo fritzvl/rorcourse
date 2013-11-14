8552d84c9c28fc0398efbddfdb0dd1a76305a407
Bogdan Sergiienko <bogdan@masterofcode.com>
2013-11-14 15:53:36 +0200

    Lesson 03 examples added

---
+++ 03/basic_meta.rb
@@ -0,0 +1,59 @@
+module FunnyModule
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def make_funny(*attributes)
+
+      attributes.each do |attribute|
+        #Important!!! Class context for self
+        if self.instance_methods.include?(attribute)
+
+          define_method "#{attribute}_funny" do
+            #Important!!! Class instance context for self
+            "Hahaha!!#{self.method(attribute).call}HAHAHA!!!"
+          end
+
+        else
+          raise Exception, "Attribute not found #{attribute}"
+        end
+      end
+
+    end
+  end
+
+
+end
+
+
+class MyObject
+  include FunnyModule
+
+  attr_accessor :user_name, :email, :address
+
+  make_funny :user_name, :email, :address
+
+
+  def initialize(name, email, address="someaddress")
+    self.user_name=name
+    self.email=email
+    self.address=address
+  end
+
+end
+
+object = MyObject.new("Bogdan", "bogdan@masterofcode.com")
+
+puts "Funny methods list"
+puts "==================================================================="
+puts object.class.instance_methods.select {|item| item.to_s.include? "_funny" }
+puts
+
+puts "Funny methods calls"
+puts "==================================================================="
+puts object.user_name_funny
+puts object.email_funny
+puts object.address_funny
+
---
+++ 03/blocks.rb
@@ -0,0 +1,141 @@
+# Simple block example
+def foo(a, b, &block)
+
+  puts "a parameter:#{a}"
+  puts "b parameter:#{b}"
+  yield a, b
+
+end
+
+puts "Simple block example"
+puts "==================================================================="
+
+foo(1, 2) do |a, b|
+
+  puts "a from block #{a}"
+  puts "b from block #{b}"
+
+end
+
+
+# What is block?
+def bar(a=5, &block)
+
+  puts a
+  yield
+  puts "Block is :#{block.class}"
+
+end
+
+puts "What is block?"
+puts "==================================================================="
+bar { puts "Im a block!" }
+
+# Lambdas
+puts "Lambda passed as block"
+puts "==================================================================="
+
+a = -> { puts "Im a lambda!" }
+bar &a
+
+#Procs
+puts "Proc passed as block"
+puts "==================================================================="
+b = Proc.new { puts "Im a Proc" }
+bar &b
+
+# Differences
+
+puts "Proc vs lambda"
+puts "==================================================================="
+puts
+
+# Return difference
+puts "Return difference"
+puts "==================================================================="
+
+def baz(a=5, &block)
+  puts a
+  block.call
+  puts "Block is :#{block.class}"
+end
+
+# Define lambda
+c = -> { puts "Im a lambda with return!"; return }
+baz &c
+
+# Define proc
+d = Proc.new { puts "Im a Proc"; return }
+
+begin
+  baz &d
+rescue LocalJumpError => e
+  puts "Error!!!"
+  puts "==================================================================="
+  puts e.message
+  puts e.backtrace
+  puts "==================================================================="
+end
+
+
+puts
+# Arguments difference
+puts "Arguments difference"
+puts "==================================================================="
+
+def qux(a=5, &block)
+  yield a, a+1
+end
+
+# Define lambda with 2 arguments
+e = ->(x, y) { puts "Sum from lambda is :#{x.to_i+y.to_i}" }
+
+# Define proc with 2 arguments
+f = Proc.new { |x, y| puts "Sum from proc is :#{x.to_i+y.to_i}" }
+
+puts
+puts "No missing arguments"
+puts "==================================================================="
+qux &e
+qux &f
+
+# Define proc with 3 arguments
+f = Proc.new { |x, y, z| puts "Sum from proc is :#{x.to_i+y.to_i+z.to_i}" }
+
+# Define lambda with 3 arguments
+e = ->(x, y, z) { puts "Sum from lambda is :#{x.to_i+y.to_i+z.to_i}" }
+
+puts
+puts "1 missing argument"
+puts "==================================================================="
+qux &f
+begin
+  qux &e
+rescue ArgumentError => e
+  puts "Error!!!"
+  puts "==================================================================="
+  puts e.message
+  puts e.backtrace
+  puts "==================================================================="
+end
+
+
+puts
+# Common use case of Procs and lambdas  - callbacks
+
+def odd?(value, callbacks)
+  puts "Is #{value} odd? "
+  if value.respond_to?(:odd?) && value.odd?
+     callbacks[:success].call
+   else
+     callbacks[:fail].call
+   end
+
+end
+
+puts "Callbacks example"
+puts "==================================================================="
+odd?(5,{success: Proc.new {puts "Success!"},fail: Proc.new { puts "Fail!"}})
+odd?(6,{success: Proc.new {puts "Success!"},fail: Proc.new { puts "Fail!"}})
+
+
